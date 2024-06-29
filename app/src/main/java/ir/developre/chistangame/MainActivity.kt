f1f61c73a9efc08ed412e923dcea78cf5fbf2ca9
SaraSaber <saraasaber77@gmail.com>
2024-06-29 16:24:23 +0430

    feat(ui_dialog): create ui dialog
    -create ui dialog about us

--- app/src/main/java/ir/developre/chistangame/MainActivity.kt
+++ app/src/main/java/ir/developre/chistangame/MainActivity.kt
@@ -7,7 +7,6 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Gravity
 import android.view.ViewGroup
-import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -32,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         val dialog = Dialog(this)
 //        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(R.layout.layout_dialog_win_stage)
+        dialog.setContentView(R.layout.layout_dialog_about_us)
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.window!!.setGravity(Gravity.CENTER)
         dialog.window!!.setLayout(
