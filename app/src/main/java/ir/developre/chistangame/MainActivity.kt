ee22909bcc1b0c20df1b07e2254e37af99111e00
SaraSaber <saraasaber77@gmail.com>
2024-06-30 14:54:59 +0430

    feat(nav_graph): add nav_graph in app
    -create nav_graph and start from home fragment_home

--- app/src/main/java/ir/developre/chistangame/MainActivity.kt
+++ app/src/main/java/ir/developre/chistangame/MainActivity.kt
@@ -1,19 +1,20 @@
 package ir.developre.chistangame
 
-import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
+import ir.developre.chistangame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase!!))
@@ -21,13 +22,42 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        navController = findNavController(R.id.my_nav_host_fragment)
+
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.Watery)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.friendlyFrost)
+
+
+
+//        navController.addOnDestinationChangedListener { _, destination, _ ->
+//
+//            when (destination.id) {
+//                R.id.splashFragment -> {
+//                    window.statusBarColor =
+//                        ContextCompat.getColor(this, R.color.background_main)
+//                    window.navigationBarColor =
+//                        ContextCompat.getColor(this, R.color.background_main)
+//                }
+//
+//                else -> {
+//                    window.statusBarColor =
+//                        ContextCompat.getColor(this, R.color.background_main)
+//                    window.navigationBarColor =
+//                        ContextCompat.getColor(this, R.color.background_main)
+//                }
+//            }
+//
+//        }
+
 
 //        val dialog = Dialog(this)
 ////        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
