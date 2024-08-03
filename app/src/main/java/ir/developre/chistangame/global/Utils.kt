4049d16482150ff65c3fb1f90e5ed31641d07d59
SaraSaber <saraasaber77@gmail.com>
2024-08-03 11:51:03 +0430

    perf(enter ruby to shop)

--- app/src/main/java/ir/developre/chistangame/global/Utils.kt
+++ app/src/main/java/ir/developre/chistangame/global/Utils.kt
@@ -26,11 +26,6 @@ object Utils {
     const val NUMBER_OF_COIN_FOR_HELP = 3
 
     const val NUMBER_OF_COINS_FOR_SEEING_ADS_TO_SHOP = 10
-    const val NUMBER_BUY_COIN_SHOP_1 = 50
-    const val NUMBER_BUY_COIN_SHOP_2 = 80
-    const val NUMBER_BUY_COIN_SHOP_3 = 100
-    const val NUMBER_BUY_COIN_SHOP_4 = 120
-    const val NUMBER_BUY_COIN_SHOP_5 = 150
 
 
     //................tapsell.................
@@ -39,15 +34,13 @@ object Utils {
     const val TAPSELL_GIFT_VIDEO_KEY = "66a0fa46c975af22e3bc6f62"
 
     //...................poolakey
-    const val rsaPublicKey = "MIHNMA0GCSqGSIb3DQEBAQUAA4G7ADCBtwKBrwDCjjce+d+Z+uvqokEpSVNEW1Q8OW9ekRuGuwktuh0DWfWy8EbVTnHeDAVcFrHnQ9k938JyP5J9nQc17CK+ERxxPwMsjATts7XtdAfEaBf2SUJ5Q4XKJkxXmnsOIhBWIgF0MW/5UIb94t9foqWEY3XdifcK64M5qdUO6j+/fSHoBQvzjhyFjK/KQHKXzPLn6M2OYhNvdgH2WHetK+e5KvL+Z+t8dNoy0Cj4giDIsYECAwEAAQ=="
+    const val rsaPublicKey =
+        "MIHNMA0GCSqGSIb3DQEBAQUAA4G7ADCBtwKBrwDCjjce+d+Z+uvqokEpSVNEW1Q8OW9ekRuGuwktuh0DWfWy8EbVTnHeDAVcFrHnQ9k938JyP5J9nQc17CK+ERxxPwMsjATts7XtdAfEaBf2SUJ5Q4XKJkxXmnsOIhBWIgF0MW/5UIb94t9foqWEY3XdifcK64M5qdUO6j+/fSHoBQvzjhyFjK/KQHKXzPLn6M2OYhNvdgH2WHetK+e5KvL+Z+t8dNoy0Cj4giDIsYECAwEAAQ=="
     const val productId_50 = "coin1"
-    const val productId_80 = "coin1"
-    const val productId_100 = "coin1"
-    const val productId_150 = "coin1"
-    const val productId_300 = "coin1"
-
-
-
+    const val productId_80 = "coin2"
+    const val productId_100 = "coin3"
+    const val productId_150 = "coin4"
+    const val productId_300 = "coin5"
 
 
 }
